3f7403b5a60492ad4352dc68e771bce9d456aeb4
BlusterySasha <52814854+BlusteryS@users.noreply.github.com>
2020-08-20 12:15:19 +0400

    Изменения.

--- src/BlusterySasha/LimitGM/Main.php
+++ src/BlusterySasha/LimitGM/Main.php
@@ -21,7 +21,7 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-	public function onPlace(BlockPlaceEvent $event) {
+    public function onPlace(BlockPlaceEvent $event) {
         $player = $event->getPlayer();
         if ($player->isOp() || $player->hasPermission("system.bypass")) {
             return;
@@ -30,7 +30,7 @@ class Main extends PluginBase implements Listener {
         $blacklist = [Block::FIRE, Block::LAVA, Block::ACTIVATOR_RAIL, Block::WATER, Block::HOPPER_BLOCK, Block::PACKED_ICE, Block::ICE, Block::MONSTER_SPAWNER, Block::SAND, Block::ENDER_CHEST, Block::RAIL, Block::ENCHANTMENT_TABLE, Block::ANVIL, Block::ITEM_FRAME_BLOCK, Block::SHULKER_BOX, Block::TNT, Block::DIAMOND_BLOCK, Block::IRON_BLOCK, Block::LAPIS_BLOCK, Block::EMERALD_BLOCK, Block::COAL_BLOCK, Block::UNDYED_SHULKER_BOX, Block::DIAMOND_ORE, Block::QUARTZ_ORE, Block::EMERALD_ORE, Block::COAL_ORE, Block::REDSTONE_ORE, Block::LAPIS_ORE, Block::IRON_ORE, Block::GOLD_ORE, Block::GOLD_BLOCK, Block::OBSIDIAN, Block::BEDROCK, Block::END_PORTAL_FRAME, Block::PISTON, Block::STICKY_PISTON];
         if ($player->isCreative()) {
             if (in_array($blocks, $blacklist)) {
-				$player->sendTip("§l§7Вы не можете ставить данный блок в креативе!");
+                $player->sendTip("§l§7Вы не можете ставить данный блок в креативе!");
                 $event->setCancelled();
                 $pk = new PlaySoundPacket();
                 $pk->x = $player->getX();
@@ -51,9 +51,9 @@ class Main extends PluginBase implements Listener {
         }
         $player->getInventory()->clearAll();
         $player->getArmorInventory()->clearAll();
-	    $player->sendTip("§l§7Предметы очищены, т. к. Вы перешли в другой режим!");
-		$player->addTitle("§e§lПредметы очищены§7!", " ");
-		$pk = new PlaySoundPacket();
+        $player->sendTip("§l§7Предметы очищены, т. к. Вы перешли в другой режим!");
+        $player->addTitle("§e§lПредметы очищены§7!", " ");
+        $pk = new PlaySoundPacket();
         $pk->x = $player->getX();
         $pk->y = $player->getY();
         $pk->z = $player->getZ();
@@ -72,16 +72,16 @@ class Main extends PluginBase implements Listener {
         $blacklist = [Block::WATER, Block::LAVA, Block::MONSTER_SPAWNER, Block::HOPPER_BLOCK, Block::ENDER_CHEST, Block::CRAFTING_TABLE, Block::CHEST, Block::FURNACE, Block::BURNING_FURNACE, Block::TRAPPED_CHEST, Block::ENCHANTMENT_TABLE, Block::ANVIL, Block::ITEM_FRAME_BLOCK, Block::SHULKER_BOX, Block::TNT, Block::DROPPER, Block::DISPENSER, Block::UNDYED_SHULKER_BOX];
         if ($player->isCreative()) {
             if (in_array($blocks, $blacklist)) {
-				$player->sendTip("§l§7Данный блок нельзя трогать в креативе!");
-				$pk = new PlaySoundPacket();
-				$pk->x = $player->getX();
-				$pk->y = $player->getY();
-				$pk->z = $player->getZ();
-				$pk->volume = 1;
-				$pk->pitch = 1;
-				$pk->soundName = 'cauldron.explode';
-				$player->dataPacket($pk);
-				$event->setCancelled();
+                $player->sendTip("§l§7Данный блок нельзя трогать в креативе!");
+                $pk = new PlaySoundPacket();
+                $pk->x = $player->getX();
+                $pk->y = $player->getY();
+                $pk->z = $player->getZ();
+                $pk->volume = 1;
+                $pk->pitch = 1;
+                $pk->soundName = 'cauldron.explode';
+                $player->dataPacket($pk);
+                $event->setCancelled();
             }
         }
     }
@@ -94,8 +94,8 @@ class Main extends PluginBase implements Listener {
         if ($player->isCreative()) {
             $player->getInventory()->clearAll();
             $player->getArmorInventory()->clearAll();
-			$player->addTitle("§c§lВы умерли§7!", " ");
-			$pk = new PlaySoundPacket();
+            $player->addTitle("§c§lВы умерли§7!", " ");
+            $pk = new PlaySoundPacket();
             $pk->x = $player->getX();
             $pk->y = $player->getY();
             $pk->z = $player->getZ();
@@ -112,8 +112,8 @@ class Main extends PluginBase implements Listener {
             return;
         }
         if ($player->isCreative()) {
-	        $player->sendTip("§l§7Вы не можете выбрасывать вещи в творческом режиме!");
-			$pk = new PlaySoundPacket();
+            $player->sendTip("§l§7Вы не можете выбрасывать вещи в творческом режиме!");
+            $pk = new PlaySoundPacket();
             $pk->x = $player->getX();
             $pk->y = $player->getY();
             $pk->z = $player->getZ();
@@ -133,15 +133,15 @@ class Main extends PluginBase implements Listener {
             }
             if ($player instanceof Player) {
                 if ($player->isCreative()) {
-					$player->sendTip("§l§7Вы не можете бить игроков в креативе!");
-					$pk = new PlaySoundPacket();
-					$pk->x = $player->getX();
-					$pk->y = $player->getY();
-					$pk->z = $player->getZ();
-					$pk->volume = 1;
-					$pk->pitch = 1;
-					$pk->soundName = 'cauldron.explode';
-					$player->dataPacket($pk);
+                    $player->sendTip("§l§7Вы не можете бить игроков в креативе!");
+                    $pk = new PlaySoundPacket();
+                    $pk->x = $player->getX();
+                    $pk->y = $player->getY();
+                    $pk->z = $player->getZ();
+                    $pk->volume = 1;
+                    $pk->pitch = 1;
+                    $pk->soundName = 'cauldron.explode';
+                    $player->dataPacket($pk);
                     $event->setCancelled();
                 }
             }
