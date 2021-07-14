3846798d674a2160fc7f857315578b3a3a9fe1bd
BlusterySasha <52814854+BlusteryS@users.noreply.github.com>
2021-07-14 17:46:29 +0400

    4.0.0 update

--- src/BlusterySasha/LimitGM/listeners/EventListener.php
+++ src/BlusterySasha/LimitGM/listeners/EventListener.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace BlusterySasha\LimitGM\listeners;
 
-use pocketmine\block\Block;
+use pocketmine\block\BlockLegacyIds;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
@@ -13,7 +13,7 @@ use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\event\player\PlayerGameModeChangeEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
 class EventListener implements Listener {
 	public function onPlace(BlockPlaceEvent $event) : void {
@@ -21,10 +21,10 @@ class EventListener implements Listener {
 		if ($player->hasPermission("limitgm.bypass")) {
 			return;
 		}
-		$blacklist = [Block::FIRE, Block::LAVA, Block::ACTIVATOR_RAIL, Block::WATER, Block::HOPPER_BLOCK, Block::PACKED_ICE, Block::ICE, Block::MONSTER_SPAWNER, Block::SAND, Block::ENDER_CHEST, Block::RAIL, Block::ENCHANTMENT_TABLE, Block::ANVIL, Block::ITEM_FRAME_BLOCK, Block::SHULKER_BOX, Block::TNT, Block::DIAMOND_BLOCK, Block::IRON_BLOCK, Block::LAPIS_BLOCK, Block::EMERALD_BLOCK, Block::COAL_BLOCK, Block::UNDYED_SHULKER_BOX, Block::DIAMOND_ORE, Block::QUARTZ_ORE, Block::EMERALD_ORE, Block::COAL_ORE, Block::REDSTONE_ORE, Block::LAPIS_ORE, Block::IRON_ORE, Block::GOLD_ORE, Block::GOLD_BLOCK, Block::OBSIDIAN, Block::BEDROCK, Block::END_PORTAL_FRAME, Block::PISTON, Block::STICKY_PISTON];
+		$blacklist = [BlockLegacyIds::FIRE, BlockLegacyIds::LAVA, BlockLegacyIds::ACTIVATOR_RAIL, BlockLegacyIds::WATER, BlockLegacyIds::HOPPER_BLOCK, BlockLegacyIds::PACKED_ICE, BlockLegacyIds::ICE, BlockLegacyIds::MONSTER_SPAWNER, BlockLegacyIds::SAND, BlockLegacyIds::ENDER_CHEST, BlockLegacyIds::RAIL, BlockLegacyIds::ENCHANTMENT_TABLE, BlockLegacyIds::ANVIL, BlockLegacyIds::ITEM_FRAME_BLOCK, BlockLegacyIds::SHULKER_BOX, BlockLegacyIds::TNT, BlockLegacyIds::DIAMOND_BLOCK, BlockLegacyIds::IRON_BLOCK, BlockLegacyIds::LAPIS_BLOCK, BlockLegacyIds::EMERALD_BLOCK, BlockLegacyIds::COAL_BLOCK, BlockLegacyIds::UNDYED_SHULKER_BOX, BlockLegacyIds::DIAMOND_ORE, BlockLegacyIds::QUARTZ_ORE, BlockLegacyIds::EMERALD_ORE, BlockLegacyIds::COAL_ORE, BlockLegacyIds::REDSTONE_ORE, BlockLegacyIds::LAPIS_ORE, BlockLegacyIds::IRON_ORE, BlockLegacyIds::GOLD_ORE, BlockLegacyIds::GOLD_BLOCK, BlockLegacyIds::OBSIDIAN, BlockLegacyIds::BEDROCK, BlockLegacyIds::END_PORTAL_FRAME, BlockLegacyIds::PISTON, BlockLegacyIds::STICKY_PISTON];
 		if ($player->isCreative() && in_array($event->getBlock()->getId(), $blacklist)) {
 			$player->sendTip("§l§7Вы не можете ставить данный блок в креативе!");
-			$event->setCancelled();
+			$event->cancel();
 			$this->sendExplode($player);
 		}
 	}
@@ -46,10 +46,10 @@ class EventListener implements Listener {
 		if (!$player->isCreative() || $player->hasPermission("limitgm.bypass")) {
 			return;
 		}
-		$blacklist = [Block::WATER, Block::LAVA, Block::MONSTER_SPAWNER, Block::HOPPER_BLOCK, Block::ENDER_CHEST, Block::CRAFTING_TABLE, Block::CHEST, Block::FURNACE, Block::BURNING_FURNACE, Block::TRAPPED_CHEST, Block::ENCHANTMENT_TABLE, Block::ANVIL, Block::ITEM_FRAME_BLOCK, Block::SHULKER_BOX, Block::TNT, Block::DROPPER, Block::DISPENSER, Block::UNDYED_SHULKER_BOX];
+		$blacklist = [BlockLegacyIds::WATER, BlockLegacyIds::LAVA, BlockLegacyIds::MONSTER_SPAWNER, BlockLegacyIds::HOPPER_BLOCK, BlockLegacyIds::ENDER_CHEST, BlockLegacyIds::CRAFTING_TABLE, BlockLegacyIds::CHEST, BlockLegacyIds::FURNACE, BlockLegacyIds::BURNING_FURNACE, BlockLegacyIds::TRAPPED_CHEST, BlockLegacyIds::ENCHANTMENT_TABLE, BlockLegacyIds::ANVIL, BlockLegacyIds::ITEM_FRAME_BLOCK, BlockLegacyIds::SHULKER_BOX, BlockLegacyIds::TNT, BlockLegacyIds::DROPPER, BlockLegacyIds::DISPENSER, BlockLegacyIds::UNDYED_SHULKER_BOX];
 		if (in_array($event->getBlock()->getId(), $blacklist)) {
 			$player->sendTip("§l§7Данный блок нельзя трогать в креативе!");
-			$event->setCancelled();
+			$event->cancel();
 			$this->sendExplode($player);
 		}
 	}
@@ -71,7 +71,7 @@ class EventListener implements Listener {
 			return;
 		}
 		$player->sendTip("§l§7Вы не можете выбрасывать вещи в творческом режиме!");
-		$event->setCancelled();
+		$event->cancel();
 		$this->sendExplode($player);
 	}
 
@@ -81,18 +81,19 @@ class EventListener implements Listener {
 			return;
 		}
 		$player->sendTip("§l§7Вы не можете бить игроков в креативе!");
-		$event->setCancelled();
+		$event->cancel();
 		$this->sendExplode($player);
 	}
 
 	private function sendExplode(Player $player) : void {
+		$loc = $player->getPosition()->asVector3();
 		$pk = new PlaySoundPacket();
-		$pk->x = $player->getX();
-		$pk->y = $player->getY();
-		$pk->z = $player->getZ();
+		$pk->x = $loc->x;
+		$pk->y = $loc->y;
+		$pk->z = $loc->z;
 		$pk->volume = 1;
 		$pk->pitch = 1;
 		$pk->soundName = "cauldron.explode";
-		$player->dataPacket($pk);
+		$player->getNetworkSession()->sendDataPacket($pk);
 	}
 }
